185eacc22e428bd6e3764b62e5a49d948eeb9e51
Chaitanya Kumar Pamarthi <p.chaitanyakumar27@gmail.com>
2025-08-12 14:47:18 +0530

    Add files via upload

---
+++ Stringbuffer.java
@@ -0,0 +1,13 @@
+package Stringsdemo;
+
+public class Stringbuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer x= new StringBuffer("Hello! ");
+		x.append("JAVA");
+		System.out.println(x);
+
+	}
+
+}
